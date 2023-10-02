62670bf4bbefe03b1a133c60a553e9dd6b19a53c
Eric Fei <eric.fei@partipost.com>
2023-10-02 22:25:08 +0800

    add post and comment models

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 
 # Create your models here.
 class Post(models.Model):
-    auther = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    author = models.ForeignKey("auth.User")
     title = models.CharField(max_length=50)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
@@ -14,3 +14,30 @@ class Post(models.Model):
     def publish(self):
         self.publish_date = timezone.now()
         self.save()
+
+    def approve_comment(self):
+        return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse("post_details", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey("blog.Post", related_name="comments")
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_lists")
+
+    def __str__(self):
+        return self.text
